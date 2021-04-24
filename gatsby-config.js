9638b991c8ac02d7eb38e063501c061fd1c15c55
pixelstrolch <email@pixelstrol.ch>
2021-04-24 12:50:03 +0200

    gatsby-source-google-spreadsheets credentials

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,15 +33,24 @@ module.exports = {
         },
       },
     },
-    // {
-    //   resolve: 'gatsby-source-google-spreadsheets',
-    //   options: {
-    //     spreadsheetId: '1p92aEK19ttxisfSZOux_qHJyPXyF04wybkYZdpLgEwA',
-    //     // apiKey: '109415772302064996163'
-    //     // Or
-    //     credentials: require('./spreadsheet-bd107c22a3e1.json')
-    //   }
-    // },
+    {
+      resolve: 'gatsby-source-google-spreadsheets',
+      options: {
+        spreadsheetId: process.env.SPREADSHEET_ID,
+        credentials: {
+          type: 'service_account',
+          project_id: process.env.PROJECT_ID,
+          private_key_id: process.env.PRIVATE_KEY_ID,
+          private_key: process.env.PRIVATE_KEY.replace(/(\\r)|(\\n)/g, '\n'),
+          client_email: process.env.CLIENT_EMAIL,
+          client_id: '109415772302064996163',
+          auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+          token_uri: 'https://oauth2.googleapis.com/token',
+          auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+          client_x509_cert_url: `https://www.googleapis.com/robot/v1/metadata/x509/${process.env.PROJECT_ID}%40appspot.gserviceaccount.com`,
+        },
+      }
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
