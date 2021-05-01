aab099b2ab15ac27467bc8945e53d0885056d159
pixelstrolch <email@pixelstrol.ch>
2021-05-01 16:18:19 +0200

    Add SEO component; auto format with prettier

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,14 @@
-import React from "react"
-import Helmet from 'react-helmet';
-import Layout from "../components/layout"
+import React from "react";
 import { Link } from "gatsby";
+
+import Seo from "../components/seo";
+import Layout from "../components/layout";
 
 const notFound = () => {
   return (
     <Layout>
-      <Helmet>
-        <title>Page not found</title>
-      </Helmet>
-      <div style={{textAlign: "center", padding:"5vh 0", lineHeight: "1.5"}}>
+      <Seo>title=Seite nicht gefunden</Seo>
+      <div style={{ textAlign: "center", padding: "5vh 0", lineHeight: "1.5" }}>
         <svg
           width="256"
           height="256"
@@ -37,10 +36,13 @@ const notFound = () => {
             fill="currentColor"
           />
         </svg>
-        <p>Either you are in a wrong page or you have lost. Lets go back <Link to="/">home</Link> safely</p>
+        <p>
+          Either you are in a wrong page or you have lost. Lets go back{" "}
+          <Link to="/">home</Link> safely
+        </p>
       </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default notFound
+export default notFound;
