8b7e53366565172f1926edd4021cfd074c9c6210
pixelstrolch <email@pixelstrol.ch>
2021-06-16 21:22:57 +0200

    Fix input fields

--- src/components/newsletter.js
+++ src/components/newsletter.js
@@ -4,18 +4,18 @@ import * as style from './newsletter.module.css'
 
 const Newsletter = () => (
   <div className={style.newsletter}>
-    <form action="https://familienverein-wahlen.us20.list-manage.com/subscribe/post?u=b253eee6416ea9da8c26e4732&amp;id=a800f2e22b" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <form action="https://familienverein-wahlen.us20.list-manage.com/subscribe/post?u=b253eee6416ea9da8c26e4732&amp;id=a800f2e22b" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
       <div class="formElement4-4">
-        <label for="mce-EMAIL">E-Mail-Adresse </label>
-        <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" />
+        <label htmlFor="mce-EMAIL">E-Mail-Adresse </label>
+        <input type="email" name="EMAIL" class="required email" id="mce-EMAIL" />
       </div>
       <div class="formElement2-4">
-        <label for="mce-FNAME">Vorname </label>
-        <input type="text" value="" name="FNAME" class="" id="mce-FNAME" />
+        <label htmlFor="mce-FNAME">Vorname </label>
+        <input type="text" name="FNAME" class="" id="mce-FNAME" />
       </div>
       <div class="formElement2-4">
-        <label for="mce-LNAME">Nachname </label>
-        <input type="text" value="" name="LNAME" class="" id="mce-LNAME" />
+        <label htmlFor="mce-LNAME">Nachname </label>
+        <input type="text" name="LNAME" class="" id="mce-LNAME" />
       </div>
       <div className={style.hidden} aria-hidden="true">
         <input type="text" name="b_b253eee6416ea9da8c26e4732_a800f2e22b" tabindex="-1" value="" />
